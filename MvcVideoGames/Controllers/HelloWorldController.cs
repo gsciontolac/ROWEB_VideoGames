538910b8eca039b0fbd4d6798e452cd9c1451388
Gabriela <gsciontolac@gmail.com>
2023-06-11 21:49:43 +0300

    Add controller, view and CSS button style

---
+++ MvcVideoGames/Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcVideoGames.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();//"This is my default action...";
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        /*
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }*/
+
+        public string Welcome1(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
